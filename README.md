0739258905dab0ecb50ceb231b84765aac61c45c
nurananacafova <nurananacafova2001@gmail.com>
2024-11-05 17:00:58 +0400

    add swagger to the services

---
+++ README.md
@@ -0,0 +1,38 @@
+# Microservice Shopping App
+
+Services are:
+
+* product-service
+* order-service
+* inventory-service
+* Api Gateway
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Getting Started](#getting-started)
+3. [Built With](#built-with)
+
+## Installation
+
+```
+git clone https://github.com/nurananacafova/Microservice-Shopping-Project.git
+```
+
+## Getting Started
+
+* Run the project.
+#### Swagger of product service: http://localhost:8080/swagger-ui.html
+#### Swagger of order service: http://localhost:8081/swagger-ui.html
+#### Swagger of inventory service: http://localhost:8082/swagger-ui.html
+#### Swagger of api gateway service: http://localhost:9000/swagger-ui.html
+
+
+
+## Built With
+
+- Spring Boot
+- Maven
+- PostgreSQL
+- MongoDB
+- Keycloak
--- api-gateway/src/main/java/com/example/apigateway/routes/Routes.java
+++ api-gateway/src/main/java/com/example/apigateway/routes/Routes.java
@@ -6,28 +6,54 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.*;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
     // We are using different programming model:
     // Functional Endpoint Programming Model
     @Bean
-    public RouterFunction<ServerResponse> productServiceRoute(){
+    public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product_service")
                 .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> orderServiceRoute(){
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(setPath("/api-docs")) //-> http://localhost:8080/api-docs
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
         return GatewayRouterFunctions.route("order_service")
                 .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> inventoryServiceRoute(){
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/api-docs")) //-> http://localhost:8081/api-docs
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return GatewayRouterFunctions.route("inventory_service")
                 .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(setPath("/api-docs")) //-> http://localhost:8082/api-docs
                 .build();
     }
 }
